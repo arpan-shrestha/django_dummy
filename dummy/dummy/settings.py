eb6f10f75434784cb2dfcf4eb9d9faf0462a6d70
Arpan Shrestha <arpanshrestha12@gmail.com>
2025-08-28 16:03:12 +0545

    vercel config

--- dummy/dummy/settings.py
+++ dummy/dummy/settings.py
@@ -69,7 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'dummy.wsgi.application'
-WSGI_APPLICATION = 'dummy.wsgi.app'
 
 
 # Database
@@ -118,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
